e26d8567692d9860ab115b9d974ff7cb71b6e708
rwilhel <rwilhel@gmail.com>
2017-09-08 11:39:16 -0600

    Fixes made to syntax errors in Vue .js file

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@ let coinMarketApi = 'https://api.coinmarketcap.com'
 let update_interval = 60 * 1000
 
 let app = new Vue ({
-  el: "#app",
+  el: '#app',
   data: {
     coins: []
   },
@@ -10,14 +10,20 @@ let app = new Vue ({
     getCoins: () => {
       let self = this;
 
-      axios.get(coinMarketApi + "/v1/ticker/?limit=20")
+      axios.get(coinMarketApi + '/v1/ticker/?limit=20')
       .then((response) => {
         this.coins = response.data
       })
       .catch((err) => {
         console.error(err)
-      }
+      })
     }
+  }
+  created: () => {
+    this.getCoins()
+  }
+  getColor: (num) => {
+    return num > 0 ? 'color: green' : 'color:red'
   }
 })
 
